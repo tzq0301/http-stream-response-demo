9fa78bdcdfd72d1c8d57a529878f7a28b333fff2
Tong Zhaoqi <tongzhaoqi@homelive.onaliyun.com>
2023-08-11 17:52:18 +0800

    .gitignore

--- client/main.go
+++ client/main.go
@@ -2,17 +2,12 @@ package main
 
 import (
 	"bufio"
-	"errors"
 	"log"
 	"net/http"
 )
 
 func main() {
 	resp, _ := http.Get("http://localhost:8080/")
-
-	if len(resp.TransferEncoding) == 0 {
-		panic(errors.New("不是 chunked 编码"))
-	}
 
 	scanner := bufio.NewScanner(resp.Body)
 	for scanner.Scan() {
